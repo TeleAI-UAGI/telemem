b7dd52bf149a3d46c3d148d8084ecf5599b8e3be
chrislin0621 <1751039@tongji.edu.cn>
2025-12-18 18:00:17 +0800

    fix: rag/run.sh

--- baselines/rag/run.sh
+++ baselines/rag/run.sh
@@ -1,14 +1,14 @@
 echo "===>  Start setting environment"
 
-export OPENAI_BASE_URL="http://117.145.68.73:20091/v1"
-export MODEL="qwen3-8b"
-export EMBED_BASE_URL="http://117.145.68.73:20416/v1"
-export EMBEDDING_MODEL="qwen3-8b-embedding"
-export OPENAI_API_KEY="EMPTY"  #"sk-55091139dcfa4d21a2d55d5387f29b43"
+export OPENAI_BASE_URL=""
+export MODEL=""
+export EMBED_BASE_URL=""
+export EMBEDDING_MODEL=""
+export OPENAI_API_KEY="sk-xxx"
 
 echo "===>  Setting Done!"
 
 python run.py \
   --input ../../data/locomo/ZH-4O_locomo_format.json \
-  --output ./rag_results_ZH_qwen3-8b.json \
+  --output ./rag_results.json \
   --chunk_size 500 --k 1
